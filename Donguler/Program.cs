4b6874fb43205d129c13af8654c93d0b658dcf7f
HÜSEYİONUR ÇETİNKAYA <HSEYONUR ETNKAYA@DESKTOP-LJM861F>
2021-01-17 22:17:45 +0300

    16 Ocak Ders Sonu

---
+++ Donguler/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Kursu";
+            string kurs2 = "Programlamaya Giriş Kursu";
+            string kurs3 = "Java";
+            //array - dizi
+            string[] kurslar = new string[] { "Yazılım Geliştirici Kursu" , "Programlamaya Giriş Kursu", "Java","Python","C#"};  
+            for (int i = 0; i<kurslar.Length ; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("For Bitti.");
+                
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+            Console.WriteLine("Sayfa Sonu - Footer");
+
+        }
+
+    }
+}
---
+++ Kampintro/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Kampintro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapisMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu.");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu.");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu.");
+            }
+
+
+            if (sistemeGirisYapisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");   
+            }
+
+
+            Console.WriteLine(kategoriEtiketi);
+
+
+
+
+        }
+    }
+}
