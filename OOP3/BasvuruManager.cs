a46c1e0b6051f56770cb75ea2d313f3f06ea670d
HÜSEYİONUR ÇETİNKAYA <HSEYONUR ETNKAYA@DESKTOP-LJM861F>
2021-01-23 23:55:58 +0300

    23 Ocak Ders Sonu

---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        //method injection
+        public void BasvuruYap(IKrediManager krediManager,List<ILoggerService> loggerServices)
+        {
+            //Başvuru bilgilerini degerlendirme
+            //
+            krediManager.Hesapla();
+            foreach (var loggerService in loggerServices)
+            {
+                loggerService.Log();
+            }
+
+        }
+        public void KrediOnBilgilenirmesiYap(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+            }
+        }
+    }
+}
