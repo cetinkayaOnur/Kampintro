723e0346915d295ad2ea78fe348f21583c59b2b1
HÜSEYİONUR ÇETİNKAYA <HSEYONUR ETNKAYA@DESKTOP-LJM861F>
2021-01-23 19:32:36 +0300

    ödev

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int> sayilar = new MyDictionary<int>();
+            sayilar.Add(1);
+            sayilar.Add(2);
+            Console.WriteLine(sayilar.Lenght);
+            sayilar.Add(3);
+            sayilar.Add(4);
+            sayilar.Add(5);
+            Console.WriteLine(sayilar.Lenght);
+            
+
+        }
+    }
+}
